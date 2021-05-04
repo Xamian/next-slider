78c5258fb0e03415f52105c96563409bb490eb8c
Max Kim Stagsted <max@max-it.dk>
2021-05-04 17:14:17 +0200

    attribution

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,9 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
+        <a href="https://thispersondoesnotexist.com/">Image from https://thispersondoesnotexist.com/</a>
+      </footer>
+      <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
